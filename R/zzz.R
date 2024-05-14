972818ddaf484f2a73f74becd7cd0e2714457869
Sebastian Fischer <sebf.fischer@gmail.com>
2024-05-14 10:01:10 +0200

    only add same_other_sizes cv to mlr_resamplings

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,6 @@
 register_mlr3 = function() {
   mlr_resamplings = utils::getFromNamespace("mlr_resamplings", ns = "mlr3")
-  mlr_resamplings$add("variable_size_train_cv", function() ResamplingVariableSizeTrainCV$new())
-  mlr_resamplings$add("same_other_sizes_cv", function() ResamplingSameOtherSizesCV$new())
+  mlr_resamplings$add("same_other_sizes_cv", ResamplingSameOtherSizesCV)
 }
 
 .onLoad = function(libname, pkgname) { # nolint
@@ -14,4 +13,4 @@ register_mlr3 = function() {
   mlr3misc::register_namespace_callback(pkgname, "mlr3", register_mlr3)
 }
 
-leanify_package()
+mlr3misc::leanify_package()
