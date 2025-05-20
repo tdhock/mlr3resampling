109ce2f6b8795f1663c486cd05a19c414ea76955
Toby Dylan Hocking <toby.hocking@r-project.org>
2025-05-20 14:31:54 +0200

    add back .sample

--- R/ResamplingBase.R
+++ R/ResamplingBase.R
@@ -24,5 +24,17 @@ ResamplingBase = R6::R6Class(
     iters = function(rhs) {
       nrow(self$instance$iteration.dt)
     }
+  ),
+  private=list(
+    .sample = function(ids, ...) {
+      data.table(
+        row_id = ids,
+        fold = sample(
+          seq(0, length(ids)-1) %%
+            as.integer(self$param_set$values$folds) + 1L
+        ),
+        key = "fold"
+      )
+    }
   )
 )
