fa319534ae465753adf5e7ba3b8a22aab0ef4ce5
Toby Dylan Hocking <toby.hocking@r-project.org>
2025-05-21 16:01:14 +0200

    = instead of <-

--- R/ResamplingBase.R
+++ R/ResamplingBase.R
@@ -22,7 +22,7 @@ ResamplingBase = R6::R6Class(
     },
     instantiate = function(task) {
       task = mlr3::assert_task(mlr3::as_task(task))
-      self$instance <- self$get_instance(task)
+      self$instance = self$get_instance(task)
       self$task_hash = task$hash
       self$task_nrow = task$nrow
       self$task_row_hash = task$row_hash
