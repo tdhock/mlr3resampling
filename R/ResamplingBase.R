6a5c2dc1370db7132b54eed8ca5750b864af7c2d
Toby Dylan Hocking <toby.hocking@r-project.org>
2025-05-21 18:32:59 +0200

    rm print and instantiate

--- R/ResamplingBase.R
+++ R/ResamplingBase.R
@@ -2,25 +2,6 @@ ResamplingBase = R6::R6Class(
   "ResamplingBase",
   inherit = mlr3::Resampling,
   public = list(
-    instantiate = function(task) {
-      task = mlr3::assert_task(mlr3::as_task(task))
-      self$instance = self$get_instance(task)
-      self$task_hash = task$hash
-      self$task_nrow = task$nrow
-      self$task_row_hash = task$row_hash
-      invisible(self)
-    },
-    print = function(...) {
-      cat(
-        format(self),
-        if (is.null(self$label) || is.na(self$label))
-          "" else paste0(": ", self$label)
-      )
-      cat("\n* Iterations:", self$iters)
-      cat("\n* Instantiated:", self$is_instantiated)
-      cat("\n* Parameters:\n")
-      str(self$param_set$values)
-    },
     train_set = function(i) {
       self$instance$iteration.dt$train[[i]]
     },
