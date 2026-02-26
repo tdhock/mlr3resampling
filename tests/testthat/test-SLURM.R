d7e77342ada128278c51eb28c8d7bd82d5b71090
Toby Dylan Hocking <toby.hocking@r-project.org>
2026-02-25 21:47:13 -0500

    identical 1L

--- tests/testthat/test-SLURM.R
+++ tests/testthat/test-SLURM.R
@@ -47,7 +47,7 @@ test_that("submit works", {
     Sys.sleep(1)
   }
   result_dt <- fread(results.csv)
-  expect_equal(sum(result_dt[["process"]]==1), 72)
+  expect_identical(result_dt[["process"]], rep(1L, 72))
   expect_equal(nrow(result_dt), 72)
   expect_true(file.exists(file.path(pkg.proj.dir, "MPI.logs", "0.log")))
 })
