01315bb3000ad51d03277f4c1ee9b45a47d313d7
Toby Dylan Hocking <toby.hocking@r-project.org>
2026-03-11 00:26:30 -0400

    NULL

--- R/score.R
+++ R/score.R
@@ -22,7 +22,7 @@ score <- function(bench.result, ...){
 }
 
 plot.score <- function(x, ..., value.var=NULL){
-  value <- Train_subsets <- NULL
+  value <- Train_subsets <- n.train.groups <- NULL
   if(requireNamespace("ggplot2")){
     if(is.null(value.var)){
       value.var <- grep("classif|regr", names(x), value=TRUE)[1]
