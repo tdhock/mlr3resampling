4cfa6a19a27f1ac31c1e225dd4f6d655e83feab3
Toby Dylan Hocking <toby.dylan.hocking@usherbrooke.ca>
2026-04-10 16:12:49 -0400

    expect error if no downsamples computed

--- tests/testthat/test-CRAN-pvalue-downsample.R
+++ tests/testthat/test-CRAN-pvalue-downsample.R
@@ -112,3 +112,118 @@ test_that("pvalue_downsample end-to-end with real SOAK sizes=0 result", {
   down.plot <- plot(down.list)
   expect_s3_class(down.plot, "ggplot")
 })
+
+test_that("pvalue_downsample fails without downsamples", {
+
+  library(data.table)
+  N <- 2400
+  abs.x <- 3*pi
+  set.seed(2)
+  grid.dt <- data.table(
+    x=seq(-abs.x,abs.x, l=201),
+    y=0)
+  x.vec <- runif(N, -abs.x, abs.x)
+  standard.deviation.vec <- c(
+    easy=0.1,
+    hard=1.7)
+  reg.data.list <- list()
+  grid.signal.dt.list <- list()
+  sim_fun <- sin
+  for(difficulty in names(standard.deviation.vec)){
+    standard.deviation <- standard.deviation.vec[[difficulty]]
+    signal.vec <- sim_fun(x.vec)
+    y <- signal.vec+rnorm(N,sd=standard.deviation)
+    task.dt <- data.table(x=x.vec, y)
+    reg.data.list[[difficulty]] <- data.table(difficulty, task.dt)
+    grid.signal.dt.list[[difficulty]] <- data.table(
+      difficulty,
+      algorithm="ideal",
+      x=grid.dt$x,
+      y=sim_fun(grid.dt$x))
+  }
+  (reg.data <- rbindlist(reg.data.list))
+  (grid.signal.dt <- rbindlist(grid.signal.dt.list))
+  algo.colors <- c(
+    featureless="blue",
+    rpart="red",
+    ideal="black")
+  if(require(ggplot2)){
+    ggplot()+
+      theme_bw()+
+      geom_point(aes(
+        x, y),
+        fill="white",
+        color="grey",
+        data=reg.data)+
+      geom_line(aes(
+        x, y, color=algorithm),
+        linewidth=2,
+        data=grid.signal.dt)+
+      scale_color_manual(values=algo.colors)+
+      facet_grid(. ~ difficulty, labeller=label_both)
+  }
+  
+  (reg.learner.list <- list(
+    if(requireNamespace("rpart"))mlr3::LearnerRegrRpart$new()))
+  SOAKED <- mlr3resampling::ResamplingSameOtherSizesCV$new()
+  SOAKED$param_set$values$folds <- 10
+  set.seed(1)
+  sim.meta.list <- list(
+    different=rbind(
+      reg.data[difficulty=="easy"][sample(.N, 400)],
+      reg.data[difficulty=="hard"][sample(.N, 200)]
+    )[, .(x,y,Subset=ifelse(difficulty=="easy", "large", "small"))],
+    iid_easy=reg.data[
+      difficulty=="easy"
+    ][sample(.N, 120)][
+    , Subset := rep(c("large","large","small"), l=.N)
+    ][, .(x,y,Subset)])
+  d_task_list <- list()
+  gg_list <- list()
+  for(sim.name in names(sim.meta.list)){
+    sim.i.dt <- sim.meta.list[[sim.name]]
+    out.csv <- sprintf("~/data_Regr/%s.csv", sim.name)
+    if(interactive()){
+      dir.create(dirname(out.csv), showWarnings = FALSE)
+      fwrite(sim.i.dt[, .(Subset, y, x)], out.csv)
+    }
+    sub_task <- mlr3::TaskRegr$new(
+      sim.name, sim.i.dt, target="y")
+    sub_task$col_roles$subset <- "Subset"
+    sub_task$col_roles$feature <- "x"
+    d_task_list[[sim.name]] <- sub_task
+    if(require("ggplot2")){
+      gg_list[[sim.name]] <- ggplot()+
+        ggtitle(paste("Simulation:", sim.name))+
+        geom_point(aes(
+          x, y),
+          color="black",
+          fill="white",
+          data=sim.i.dt)+
+        geom_line(aes(
+          x, y, color=algorithm),
+          data=grid.signal.dt)+
+        scale_color_manual(values=algo.colors)+
+        facet_grid(Subset~., labeller=label_both)
+    }
+  }
+  gg_list
+  (reg.bench.grid <- mlr3::benchmark_grid(
+    d_task_list$iid_easy,
+    reg.learner.list,
+    SOAKED))
+  (reg.bench.result <- mlr3::benchmark(reg.bench.grid))
+
+  (score_dt <- mlr3resampling::score(
+    reg.bench.result, mlr3::msr("regr.rmse")
+  )[, .(
+    test.subset, test.fold,
+    train.subsets, Train_subsets, groups, n.train.groups,
+    algorithm, regr.rmse, task_id)])
+  if(interactive())plot(score_dt)
+  plist <- mlr3resampling::pvalue(score_dt)
+  if(interactive())plot(plist)
+  expect_error({
+    mlr3resampling::pvalue_downsample(score_dt)
+  }, "no downsample results, please set SOAK$param_set$sizes=0 and re-run benchmark")
+})
