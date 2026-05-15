02b9f9f0c9a37ad32c793ddda5db2f3ae088db3b
Toby Dylan Hocking <toby.hocking@r-project.org>
2026-05-14 22:34:34 -0400

    number vignettes

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ cp -r $PKG $RELEASE
 
 echo Editing $RELEASE for CRAN submission
 grep -v Remotes $PKG/DESCRIPTION > $RELEASE/DESCRIPTION
-rm -r $RELEASE/tests/testthat/*
+rm -r $RELEASE/tests/testthat/* $RELEASE/vignettes/*resamplers.Rmd $RELEASE/vignettes/*Reproducibility.Rmd
 cp $PKG/tests/testthat/test-CRAN*.R $RELEASE/tests/testthat
 
 echo Building $RELEASE
